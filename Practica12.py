9904d0673cf0d10882e44ec25f469af99a24b3c1
Jesus Almaguer <111038018+b8upq@users.noreply.github.com>
2023-03-03 10:38:22 -0600

    Interfaz grafica completamente funcional

--- Practica12.py
+++ Practica12.py
@@ -1,11 +1,28 @@
 #Importación de librerias
 from tkinter import *
+from tkinter import messagebox
 
 #Creación de la ventana
 ventana = Tk()
 ventana.title("Login")
 ventana.geometry("800x600")
 ventana.config(bg="#c2d6d6")
+ventana.resizable(width=False, height=False)
+
+#Definimos nuestras variables
+usuario = str
+contra = str
+
+
+#Definimos las funciones
+def Login():
+    usuario = correo.get()
+    contra = password.get()
+    if usuario == "bocho@123.com" and contra == "12345":
+        messagebox.showinfo('Bienvenido', "Bienvenido de nuevo.")
+    else:
+        messagebox.showerror('Error', "Correo o Contraseña incorrectos. Intentelo de nuevo.")   
+
 
 #Creación de secciones
 seccion1 = Frame(ventana, bg='#c2d6d6')
@@ -23,18 +40,16 @@ label1.pack(fill="x")
 label2 = Label(seccion1, text="",font=40, fg="black", bg='#a3c2c2')
 label2.pack(fill="x")
 
-
-
-
-correo = Entry(seccion2)
+#Creacion de los input's
+correo = Entry(seccion2, textvariable= usuario, )
 correo.pack()
 
-password = Entry(seccion2)
+password = Entry(seccion2, textvariable= contra, show="*")
 password.pack()
 
 
 #Creacion del boton
-botonLogin = Button(seccion2, text="Login", bg="#85adad", fg="#ffffff")
+botonLogin = Button(seccion2, text="Login", bg="#85adad", fg="#ffffff", cursor="hand2", command=Login)
 botonLogin.pack()
 
 
